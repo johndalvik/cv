46acf68ed02c5577ff1f2ec9fb9d1cbe26ec2771
Akos Horvath <akos.horvath@attrecto.com>
2018-08-16 13:09:27 +0200

    work

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const cleanCSS = require('gulp-clean-css')
 const rename = require('gulp-rename')
 const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
+const webpack = require('webpack')
+const config = require('./webpack.config')
 
 gulp.task('build-css', () => {
   return gulp.src('dist/*.css')
@@ -13,9 +15,26 @@ gulp.task('build-css', () => {
     .pipe(gulp.dest('dist'))
 })
 
+gulp.task('build-js', () => {
+  return buildJS()
+})
+
 gulp.task('move-css', () => {
   return gulp.src('src/css/**.css')
     .pipe(gulp.dest('dist'))
 })
 
 gulp.task('css', gulp.series('move-css', 'build-css'))
+gulp.task('build', gulp.series('build-js', 'css'))
+
+
+function buildJS() {
+  return new Promise(resolve => {
+    return webpack(config, (err, stats) => {
+      if (err) {
+        console.log('Webpack', err)
+      }
+      resolve()
+    })
+  })
+}
