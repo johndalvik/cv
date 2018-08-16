75650f3c848647be79ebfd2f9c467a7547ced5e0
Akos Horvath <akos.horvath@attrecto.com>
2018-08-16 12:05:55 +0200

    work

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
-let gulp = require('gulp');
-let cleanCSS = require('gulp-clean-css');
+const gulp = require('gulp')
+const cleanCSS = require('gulp-clean-css')
+const rename = require('gulp-rename')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
 
-gulp.task('css', () => {
-  return gulp.src('css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist'));
-});
+gulp.task('build-css', () => {
+  return gulp.src('dist/*.css')
+    .pipe(postcss([autoprefixer({browsers: ['last 2 versions', 'ie 11']})]))
+    .pipe(cleanCSS())
+    .pipe(postcss([require('css-mqpacker')()]))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('move-css', () => {
+  return gulp.src('src/css/**.css')
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('css', gulp.series('move-css', 'build-css'))
